1b106a8971494d4c7ddb812abbe4b3fa4a0e53a4
Ignacio <61211563+iribastrillo@users.noreply.github.com>
2024-11-02 19:47:31 -0300

    Update deploy.sh

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,5 +1,6 @@
 pwd
-cd ../dist/app/catalog
+cd ./frontent/dist/apps/catalog
+
 
 # Sync bundles with strong cache
 aws s3 sync ./common/favicons s3://$S3_ORIGIN_BUCKET/common/favicons --exclude "site.webmanifest" --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
